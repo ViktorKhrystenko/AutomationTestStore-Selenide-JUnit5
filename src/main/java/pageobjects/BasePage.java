81215c5325e5f42404b70abfd42104294c160147
viktorkhrystenko1 <viktor.khrystenko1@gmail.com>
2026-03-27 18:11:41 +0100

    [#3] Refactor and Jenkins integration - clicking on field before sending `Enter`.

--- src/main/java/pageobjects/BasePage.java
+++ src/main/java/pageobjects/BasePage.java
@@ -153,6 +153,7 @@ public abstract class BasePage {
         WebElement oldPageHtml = driver.findElement(ROOT_HTML_ELEMENT);
         if (isChromeInDocker()) {
             try {
+                new Actions(driver).click().perform();
                 JsActionsUtil.confirmForm(field);
             }
             catch (JsonException e) {
